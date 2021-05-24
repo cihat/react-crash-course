dc14983f7e679468460018ac02dc0008b6f81622
cihat <cihat02020202@gmail.com>
2021-05-24 18:50:02 +0300

    update components template

---
+++ birlikte-next.js-ogreniyoruz/components/Popular/index.js
@@ -0,0 +1,39 @@
+import Link from "next/link"
+import styles from './popular.module.scss'
+
+const popular = ({ movies }) => {
+  // console.log(movies)
+
+  return (
+    <div className={styles.popular}>
+      <h2>Popular Films</h2>
+      <div className={styles['popular-inner']}>
+        {movies.results.map((movie) => (
+          <Link
+            href={`/film/${movie.id}`}
+            className={styles['movie-cart']}
+            key={movie.id}
+          >
+            <a>
+              <h3>{movie.title}</h3>
+              <img
+                src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`}
+                alt=""
+              />
+            </a>
+          </Link>
+        ))}
+      </div>
+    </div> 
+  )
+}
+
+export async function getStaticProps({params}) {
+  return {
+    props: {
+      params
+    }
+  }
+}
+
+export default popular
